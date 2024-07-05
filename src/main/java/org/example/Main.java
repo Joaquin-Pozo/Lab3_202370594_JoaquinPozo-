5d458b5be8e25095faf10491768c9209a11c4a75
Joaquín Pozo <joaquin.pozo@usach.cl>
2024-07-05 00:09:44 -0400

    Implementación del método cargarTrenes para cargar trenes desde un archivo de texto.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,9 @@
 package org.example;
 
+import org.example.metro.Driver;
 import org.example.metro.Line;
 import org.example.metro.Subway;
+import org.example.metro.Train;
 import org.example.servicios.CargarArchivos;
 
 import java.util.List;
@@ -9,37 +11,56 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Subway metro = new Subway(1, "Red de Metro de Santiago");
+        Subway metro = new Subway(1, "Red de metro de Santiago");
         Scanner scanner = new Scanner(System.in);
-        int option = 0;
+        int opcion = 0;
 
-        while (option != 3) {
+        while (opcion != 4) {
             System.out.println("Menú:");
-            System.out.println("1. Cargar línea desde archivo");
-            System.out.println("2. Mostrar líneas");
-            System.out.println("3. Terminar");
+            System.out.println("1. Cargar información de la red de metro");
+            System.out.println("2. Visualizar estado actual del sistema de metro");
+            System.out.println("3. Interactuar con el sistema de metro");
+            System.out.println("4. Salir del programa");
             System.out.print("Seleccione una opción: ");
-            option = scanner.nextInt();
+            opcion = scanner.nextInt();
             scanner.nextLine(); // Limpiar el buffer
 
-            switch (option) {
+            switch (opcion) {
                 case 1:
-                    String fileName = "src/main/java/org/example/servicios/lineas.txt";
                     CargarArchivos newFile = new CargarArchivos();
-                    List<Line> lines = newFile.cargarLineas(fileName);
-                    for (Line line : lines) {
-                        metro.addLine(line);
+                    System.out.println("1. Agregar líneas");
+                    System.out.println("2. Agregar trenes con distinto número de carros");
+                    int opcionCargar = scanner.nextInt();
+                    scanner.nextLine();
+                    if (opcionCargar == 1) {
+                        String fileName = "src/main/java/org/example/servicios/lineas.txt";
+                        List<Line> lines = newFile.cargarLineas(fileName);
+                        for (Line line : lines) {
+                            metro.addLine(line);
+                        }
+                        System.out.println("Lineas cargadas exitosamente");
+                        break;
+                    } else if (opcionCargar == 2) {
+                        String fileName = "src/main/java/org/example/servicios/trenes.txt";
+                        List<Train> trains = newFile.cargarTrenes(fileName);
+                        for (Train train : trains) {
+                            metro.addTrain(train);
+                        }
+                        System.out.println("Trenes cargados exitosamente");
+                        break;
+                    } else {
+                        System.out.println("Opción no válida. Intente de nuevo");
+                        break;
                     }
-                    System.out.println("Lineas cargadas exitosamente en " + metro.getName());
-                    break;
                 case 2:
                     System.out.println(metro);
                     break;
                 case 3:
-                    System.out.println("Terminando el programa.");
                     break;
+                case 4:
+                    System.out.println("Terminando el programa...");
                 default:
-                    System.out.println("Opción no válida. Intente de nuevo.");
+                    System.out.println("Opción no válida. Intente de nuevo");
             }
         }
         scanner.close();
--- src/main/java/org/example/servicios/CargarArchivos.java
+++ src/main/java/org/example/servicios/CargarArchivos.java
@@ -1,9 +1,6 @@
 package org.example.servicios;
 
-import org.example.metro.Line;
-import org.example.metro.Section;
-import org.example.metro.Station;
-import org.example.metro.StationType;
+import org.example.metro.*;
 
 import java.io.*;
 import java.util.ArrayList;
@@ -28,18 +25,18 @@ public class CargarArchivos {
                         int cantidadSecciones = Integer.parseInt(br.readLine());
                         List<Section> sections = new ArrayList<>();
                         for (int j = 0; j < cantidadSecciones; j++) {
-                            String readSection = br.readLine();
+                            String readSection = br.readLine().trim();
                             String[] sectionParts = readSection.split(",");
-                            int id1 = Integer.parseInt(sectionParts[0]);
-                            String name1 = sectionParts[1];
-                            StationType type1 = new StationType(sectionParts[2]);
-                            int stopTime1 = Integer.parseInt(sectionParts[3]);
-                            int id2 = Integer.parseInt(sectionParts[4]);
-                            String name2 = sectionParts[5];
-                            StationType type2 = new StationType(sectionParts[6]);
-                            int stopTime2 = Integer.parseInt(sectionParts[7]);
-                            int distance = Integer.parseInt(sectionParts[8]);
-                            int cost = Integer.parseInt(sectionParts[9]);
+                            int id1 = Integer.parseInt(sectionParts[0].trim());
+                            String name1 = sectionParts[1].trim();
+                            StationType type1 = new StationType(sectionParts[2].trim());
+                            int stopTime1 = Integer.parseInt(sectionParts[3].trim());
+                            int id2 = Integer.parseInt(sectionParts[4].trim());
+                            String name2 = sectionParts[5].trim();
+                            StationType type2 = new StationType(sectionParts[6].trim());
+                            int stopTime2 = Integer.parseInt(sectionParts[7].trim());
+                            int distance = Integer.parseInt(sectionParts[8].trim());
+                            int cost = Integer.parseInt(sectionParts[9].trim());
                             Station station1 = new Station(id1, name1, type1, stopTime1);
                             Station station2 = new Station(id2, name2, type2, stopTime2);
                             Section section = new Section(station1, station2, distance, cost);
@@ -58,4 +55,49 @@ public class CargarArchivos {
         }
         return newLines;
     }
+
+    public List<Train> cargarTrenes(String fileName) {
+        List<Train> newTrains = new ArrayList<>();
+
+        try {
+            File archivo = new File(fileName);
+            if (archivo.exists()) {
+                FileReader fr = new FileReader(archivo);
+                BufferedReader br = new BufferedReader(fr);
+                String data;
+                while ((data = br.readLine()) != null) {
+                    if (data.startsWith("Tren")) {
+                        String[] parts = data.split(" ");
+                        int trainId = Integer.parseInt(parts[1]);
+                        String trainMaker = br.readLine();
+                        int speed = Integer.parseInt(br.readLine());
+                        int stationStayTime = Integer.parseInt(br.readLine());
+                        int cantidadPcars = Integer.parseInt(br.readLine());
+                        List<PassengerCar> pcars = new ArrayList<>(cantidadPcars);
+                        for (int j = 0; j < cantidadPcars; j++) {
+                            String readPcar = br.readLine().trim();
+                            String[] pcarParts = readPcar.split(",");
+                            int id = Integer.parseInt(pcarParts[0].trim());
+                            int passengerCapacity = Integer.parseInt(pcarParts[1].trim());
+                            String model = pcarParts[2].trim();
+                            String pcarTrainMaker = pcarParts[3].trim();
+                            CarType carType = new CarType(pcarParts[4].trim());
+                            PassengerCar pcar = new PassengerCar(id, passengerCapacity, model, pcarTrainMaker, carType);
+                            pcars.add(pcar);
+                        }
+                        Train newTrain = new Train(trainId, trainMaker, speed, stationStayTime, pcars);
+                        newTrains.add(newTrain);
+                    }
+                }
+                br.close();
+            } else {
+                System.out.println("El archivo " + fileName + " no existe.");
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return newTrains;
+    }
+    
+    
 }
